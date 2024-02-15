d761932b10350622efb08fe1422a7a067b4142f4
larinhab <larabviana@outlook.com>
2024-02-15 14:03:59 -0300

    :soon:

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,17 @@ import theme from './styles/theme.js'
 
 import { MoviePreviewNote } from './pages/MoviePreview'
 import { Profile } from './pages/Profile'
+import { CreateMovie } from './pages/CreateMovie'
 import { Home } from './pages/Home'
+import {SignIn} from './pages/SignIn'
+import {SignUp} from './pages/SignUp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}> 
       <GlobalStyles/>
     
-        <Home></Home>
+        <MoviePreviewNote></MoviePreviewNote>
 
     </ThemeProvider>
   </React.StrictMode>,
