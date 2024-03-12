40fb99a86392d58853d206c789cbe7b1f23e0ef8
larinhab <larabviana@outlook.com>
2024-03-11 21:22:40 -0300

    :bug: refactoring img toggler

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,48 @@
-import { Container, Form, Avatar } from './styles'
-
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
 import { ButtonBack } from '../../components/ButtonBack'
+import avatarPlaceHolder  from '../../assets/user.svg'
+import { Container, Form, Avatar } from './styles'
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+import { useNavigate } from "react-router-dom"
+import { useAuth } from '../../hooks/auth'
+import { api } from '../../service/api'
+import { useState } from "react"
 
 export function Profile() {
+    const { user, uptadeProfile } = useAuth() // pegando o user la da autenticacao
+    const [ name, setName ] = useState(user.name) //Pegando a informação do que ja esta autenticado
+    const [ email, setEmail ] = useState(user.email)
+    const [ passowordOld, setPasswordOld ] = useState()
+    const [ passwordNew, setPasswordNew ] = useState()
+
+    const avatarURL = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceHolder
+
+    const [ avatar, setAvatar ] = useState(avatarURL)
+    const [ avatarFile, setAvatarFile ] = useState(null)
+    
+    const navigate = useNavigate()
+
+    async function handleUptade(){
+        const user = { 
+            name, 
+            email,
+            password: passwordNew,
+            old_password: passowordOld
+        }
+        await uptadeProfile({ user, avatarFile })
+            
+        navigate("/")
+    }
+
+    function handleChangeAvatar(event){
+        const file = event.target.files[0]
+        setAvatarFile(file)
+
+        const imagaPreview = URL.createObjectURL(file)
+        setAvatar(imagaPreview);
+    }
+
     return(
         <Container>
             <header>
@@ -14,13 +51,14 @@ export function Profile() {
 
             <Form>
                 <Avatar>
-                    <img src="https://github.com/larinhab.png" alt="Foto do Usuário" />
+                    <img src={avatarURL} alt="Foto do Usuário" />
                     
                     <label htmlFor="avatar">
                         <FiCamera></FiCamera>
                     <input 
-                    id="text" 
-                    type="file" />
+                    id="avatar" 
+                    type="file"
+                    onChange={ handleChangeAvatar } />
 
                     </label>
                 </Avatar>
@@ -28,28 +66,34 @@ export function Profile() {
                 <Input
                 placeholder="Nome"
                 type="text"
-                icon={FiUser}>
+                icon={FiUser}
+                value={name}
+                onChange= {e => setName(e.target.value)}>
                 </Input>
 
                 <Input
                 placeholder="E-mail"
                 type="text"
-                icon={FiMail}>
+                icon={FiMail}
+                value={email}
+                onChange= {e => setEmail(e.target.value)}>
                 </Input>
 
                 <Input
                 placeholder="Senha Atual"
                 type="password"
-                icon={FiLock}>
+                icon={FiLock}
+                onChange= {e => setPasswordOld(e.target.value)}>
                 </Input>
 
                 <Input
                 placeholder="Nova senha"
                 type="password"
-                icon={FiLock}>
+                icon={FiLock}
+                onChange= {e => setPasswordNew(e.target.value)}>
                 </Input>
 
-                <Button title="Salvar"></Button>
+                <Button title="Salvar" onClick={ handleUptade }></Button>
             </Form>
         </Container>
     )
