dc225835e7b5dff99e4df8d9d32db8c42ab84172
larinhab <larabviana@outlook.com>
2024-03-14 22:35:01 -0300

    :star: fix syntax

--- src/components/Stars/styles.js
+++ src/components/Stars/styles.js
@@ -4,11 +4,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  gap: ${({ isBigSize }) => (isBigSize ? "1rem" : "0.6rem")};
+  gap: ${({ isbigSize }) => (isbigSize ? "1rem" : "0.6rem")};
   cursor: pointer;
 
   svg {
-    font-size: ${({ isBigSize }) => (isBigSize ? "2rem" : "1.2rem")};
+    font-size: ${({ isbigSize }) => (isbigSize ? "2rem" : "1.8rem")};
     color: ${({ theme }) => theme.COLORS.PINK};
   }
 `;
